61e64df41466efd92bdb7c46c1faf0e3f94e4e6c
Ella Wilkins <ellawlk@ubuntu-2004.eecis.udel.edu>
2023-12-07 11:22:38 -0500

    all kernels done

--- compute.cu
+++ compute.cu
@@ -4,52 +4,79 @@
 #include "config.h"
 #include <cuda_runtime.h>
 
-//compute: Updates the positions and locations of the objects in the system based on gravity.
-//Parameters: None
-//Returns: None
-//Side Effect: Modifies the hPos and hVel arrays with the new positions and accelerations after 1 INTERVAL
-void compute(){
-	//make an acceleration matrix which is NUMENTITIES squared in size;
-	int i,j,k;
-	vector3* values=(vector3*)malloc(sizeof(vector3)*NUMENTITIES*NUMENTITIES);
-	vector3** accels=(vector3**)malloc(sizeof(vector3*)*NUMENTITIES);
-	for (i=0;i<NUMENTITIES;i++)
-		accels[i]=&values[i*NUMENTITIES];
-	//first compute the pairwise accelerations.  Effect is on the first argument.
-	for (i=0;i<NUMENTITIES;i++){
-		for (j=0;j<NUMENTITIES;j++){
-			if (i==j) {
-				FILL_VECTOR(accels[i][j],0,0,0);
-			}
-			else{
-				vector3 distance;
-				for (k=0;k<3;k++) distance[k]=hPos[i][k]-hPos[j][k];
-				double magnitude_sq=distance[0]*distance[0]+distance[1]*distance[1]+distance[2]*distance[2];
-				double magnitude=sqrt(magnitude_sq);
-				double accelmag=-1*GRAV_CONSTANT*mass[j]/magnitude_sq;
-				FILL_VECTOR(accels[i][j],accelmag*distance[0]/magnitude,accelmag*distance[1]/magnitude,accelmag*distance[2]/magnitude);
-			}
-		}
-	}
-	//sum up the rows of our matrix to get effect on each entity, then update velocity and position.
-	for (i=0;i<NUMENTITIES;i++){
-		vector3 accel_sum={0,0,0};
-		for (j=0;j<NUMENTITIES;j++){
-			for (k=0;k<3;k++)
-				accel_sum[k]+=accels[i][j][k];
-		}
-		//compute the new velocity based on the acceleration and time interval
-		//compute the new position based on the velocity and time interval
-		for (k=0;k<3;k++){
-			hVel[i][k]+=accel_sum[k]*INTERVAL;
-			hPos[i][k]+=hVel[i][k]*INTERVAL;
-		}
-	}
-	free(accels);
-	free(values);
+__global__ void fill_vector(vector3 *vals, double *hPos, double *hVel, double *mass)
+{
+    // i = x's index * dimension * index in the thread
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+
+    // j = y's index * dimension * index in the thread
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
+
+    // k = thread index of z
+    int k = threadIdx.z;
+
+    // if i and j are equal, set vector to 0,0,0
+    if (i == j)
+    {
+        FILL_VECTOR(vals[(i * NUMENTITIES) + j], 0, 0, 0);
+    }
+    else
+    {
+        // init distance
+        vector3 distance;
+
+        // calculate distance for each
+        for (int k = 0; k < 3; k++)
+        {
+            distance[k] = hPos[i * 3 + k] - hPos[j * 3 + k];
+        };
+
+        // calculating the magnitude
+        double magnitude_sq = (distance[0] * distance[0]) + (distance[1] * distance[1]) + ((distance[2] * distance[2]));
+        double magnitude = sqrt(magnitude_sq);
+
+        // calculating magnitude of the acceleration
+        double accel_mag = -1 * GRAV_CONSTANT * mass[j] / magnitude_sq;
+
+        // fill x, y, z
+        FILL_VECTOR(vals[i * NUMENTITIES + j],
+                    accel_mag * distance[0] / magnitude, accel_mag * distance[1] / magnitude,
+                    accel_mag * distance[2] / magnitude);
+    }
 }
 
-__global__
-	void add(float**accels,vector3*hPos,float*mass){
-		//pass
-	};
+__global__ void sum_clmn(vector3 *accels, double *hPos, double *hVel, double *mass)
+{
+    // i = x's index * dimension * index in the thread
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+
+    // if i is greater than # of entities, stop
+    if (i >= NUMENTITIES)
+    {
+        return;
+    }
+    else
+    {  
+        // set total to empty (init)
+        vector3 total = {0, 0, 0};
+
+        // fill for each entity
+        for (int j = 0; j < NUMENTITIES; j++)
+        {
+            for (int k = 0; k < 3; k++)
+                total[k] += accels[i * NUMENTITIES + j][k];
+        }
+        
+        for (int k = 0; k < 3; k++)
+        {
+            hVel[i * 3 + k] += total[k] * INTERVAL;
+            hPos[i * 3 + k] += hVel[i * 3 + k] * INTERVAL;
+        }
+    }
+}
+
+void compute(vector3 *accels, double *hPos, double *hVel, double *mass)
+{
+    dim3 size(16, 16, 3);
+    int a = ((NUMENTITIES + 15) / 16);
+}
